0378d1ef025b56461574a784b80004a91983853b
Selami <selami.hoxha@uni-pr.edu>
2019-11-12 10:40:07 +0100

    Create the testing Directory

---
+++ squirrel.deduplication/src/test/java/org/dice_research/squirrel/component/DeduplicatorComponentTest.java
@@ -0,0 +1,12 @@
+package org.dice_research.squirrel.component;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DeduplicatorComponentTest {
+
+    @Test
+    public void testInt(){
+        Assert.assertTrue(1==1);
+    }
+}
